f59930131d25848438c39264a16b01b91b471821
SpiderBuh <ijesbgujdbuheagfvues@gmail.com>
2025-07-26 14:56:41 +0200

    Updated AttachToyParent.cs

--- Choas/Features/AdminToyToys/AttachToyParent.cs
+++ Choas/Features/AdminToyToys/AttachToyParent.cs
@@ -21,12 +21,12 @@ public class AttachToyParent : ParentCommand, ICustomCommand
     
     public override string[] Aliases => null;
 
-    public string[] Usage => ["net id"];
+    public string[] Usage => ["net id/subcommand"];
     public PlayerPermissions? Permission => PlayerPermissions.FacilityManagement;
     public string PermissionString => string.Empty;
     public bool RequirePlayerSender => false;
     public bool SanitizeResponse => false;
-    public override string Description => "Attaches an admin toy to an object based on the sub command, or sets the parent to null if just given a netID.";
+    public override string Description => "Attaches an admin toy to an object based on the sub command.";
 
     public override void LoadGeneratedCommands()
     {
@@ -36,6 +36,12 @@ public class AttachToyParent : ParentCommand, ICustomCommand
     
     protected override bool ExecuteParent(ArraySegment<string> arguments, ICommandSender sender, out string response)
     {
+        if (arguments.Count == 0)
+        {
+            response = $"Please provide a valid subcommand\n{string.Join("/", this.Commands.Select(r => r.Value.Command))}\nOr a NetID to detach it.";
+            return false;
+        }
+        
         if (!sender.CanRun(this, arguments, out response, out _, out _)) return false;
 
         if (!(uint.TryParse(arguments.At(0), out var toyid) &&
